1d581ead6a71282d15d94272f8db10ad0bc05605
idanass <anassbouziane09@gmail.com>
2026-03-24 20:43:55 +0000

    add model auto-download from Drive

--- predict.py
+++ predict.py
@@ -9,14 +9,27 @@ from pytorch_grad_cam.utils.image import show_cam_on_image
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 import io
 import base64
+import os
+import gdown
 
 # 1. DEVICE
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cpu')
 
-# 2. CHARGER LE MODELE
+# 2. TELECHARGER LE MODELE DEPUIS DRIVE
+MODEL_PATH = 'model/resnet50_best.pth'
+
+if not os.path.exists(MODEL_PATH):
+    os.makedirs('model', exist_ok=True)
+    print("⬇️ Téléchargement du modèle depuis Drive...")
+    gdown.download(
+        'https://drive.google.com/uc?id=1SmydCg9pb1vtad5H6R0WenMEHkOsN2Nt',
+        MODEL_PATH, quiet=False
+    )
+    print("✅ Modèle téléchargé !")
+
+# 3. CHARGER LE MODELE
 def load_model():
     model = models.resnet50(weights=None)
-    
     num_features = model.fc.in_features
     model.fc = nn.Sequential(
         nn.Linear(num_features, 256),
@@ -24,15 +37,14 @@ def load_model():
         nn.Dropout(0.4),
         nn.Linear(256, 2)
     )
-    model.load_state_dict(torch.load('model/resnet50_best.pth',
-                          map_location=device))
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
     model.to(device)
     return model
 
 model = load_model()
 print("✅ Modèle chargé !")
 
-# 3. TRANSFORM
+# 4. TRANSFORM
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
@@ -40,13 +52,11 @@ transform = transforms.Compose([
                          std=[0.229, 0.224, 0.225])
 ])
 
-# 4. PREDICTION + GRAD-CAM
+# 5. PREDICTION + GRAD-CAM
 def predict(image: Image.Image):
-    # Preprocessing
     orig_array   = np.array(image.resize((224, 224))) / 255.0
     input_tensor = transform(image).unsqueeze(0).to(device)
 
-    # Prediction
     with torch.no_grad():
         outputs = model(input_tensor)
         probs   = torch.softmax(outputs, dim=1)[0]
@@ -56,19 +66,18 @@ def predict(image: Image.Image):
     confidence = round(probs[pred].item() * 100, 2)
 
     # Grad-CAM
-    model.train()  # ← important pour les gradients !
+    model.train()
     target_layer  = [model.layer4[-1]]
     cam           = GradCAM(model=model, target_layers=target_layer)
     targets       = [ClassifierOutputTarget(pred)]
     grayscale_cam = cam(input_tensor=input_tensor, targets=targets)[0]
-    model.eval()  
-    
+    model.eval()
+
     visualization = show_cam_on_image(
         orig_array.astype(np.float32),
         grayscale_cam, use_rgb=True
     )
 
-    # Convertir heatmap en base64
     heatmap_img = Image.fromarray(visualization)
     buffer      = io.BytesIO()
     heatmap_img.save(buffer, format='PNG')
